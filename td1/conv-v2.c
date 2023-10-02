3a4710400db98e663aa72561fc3c8586d0f36dca
Romain Chardiny <romain.chardiny@gmail.com>
2023-10-02 10:42:48 +0200

    finish

--- td1/conv-v2.c
+++ td1/conv-v2.c
@@ -9,8 +9,8 @@ int main() {
     const char* sep = "+----+----+\n";
     printf("%s", sep);
     for (double c = 0; c < 20; c += 0.5) {
-        int c_int = (int)rint(c);
-        int f_int = (int)rint(celsius_to_fahrenheit(c));
+        int c_int = (int) rint(c);
+        int f_int = (int) rint(celsius_to_fahrenheit(c));
         printf("| %02d | %02d |\n", c_int, f_int);
     }
     printf("%s", sep);
---
+++ td1/wc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int c;
+    int char_count = 0;
+    int word_count = 0;
+    int line_count = 0;
+    bool was_space = false;
+
+    while ((c = getchar()) != EOF) {
+        char_count++;
+
+        if (c == ' ' || c == '\n') {
+            if (was_space) continue;
+
+            word_count++;
+            was_space = true;
+        } else {
+            was_space = false;
+        }
+
+        if (c == '\n') line_count++;
+    }
+
+    if (!was_space) {
+        word_count++;
+        line_count++;
+    }
+
+    printf("lines: %d\nwords: %d\nchars: %d\n", line_count, word_count, char_count);
+
+    return 0;
+}
