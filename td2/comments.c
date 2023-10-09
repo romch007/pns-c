ce108919f6b10866f1b54f162a3d30f539204aca
Romain Chardiny <romain.chardiny@gmail.com>
2023-10-09 08:45:49 +0200

    comments

---
+++ td2/comments.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int c;
+    bool in_comment = false;
+    bool in_str = false;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\"') {
+            in_str = !in_str;
+        }
+
+        if (!in_str) {
+            if (c == '/' && !in_comment && getchar() == '*') {
+                in_comment = true;
+                continue;
+            } if (c == '*' && in_comment && getchar() == '/') {
+                in_comment = false;
+                continue;
+            }
+        }
+
+        if (!in_comment) {
+            printf("%c", c);
+        }
+    }
+}
